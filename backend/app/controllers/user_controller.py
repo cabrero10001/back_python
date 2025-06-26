8813e57f9209e2400fb3303197dc80b34e5c994b
Santiagx2001 <ssalazarb918@gmail.com>
2025-06-25 21:37:17 -0500

    back funcionando

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -5,22 +5,23 @@ from flask import jsonify
 
 def register_user(data):
     username = data.get("username")
-    email = data.get("email")
     password = data.get("password")
 
-    if not username or not email or not password:
+    if not username or not password:
         return {"error": "Faltan campos"}, 400
 
     if User.query.filter_by(username=username).first():
         return {"error": "El usuario ya existe"}, 409
 
-    new_user = User(username=username, email=email, password=password)
+    new_user = User(username=username, password=password)
     db.session.add(new_user)
     db.session.commit()
 
     return {"message": "Usuario registrado"}, 201
 
 def login_user(data):
+    print("Iniciando sesión")
+    print(data)
     username = data.get("username")
     password = data.get("password")
 
--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -5,14 +5,12 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "username": self.username,
-            "email": self.email,
             "password": self.password
         }
 

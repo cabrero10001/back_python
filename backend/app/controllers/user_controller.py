dd0e4d3b136a963640ce19bb4e1006592aaae36e
alexs <alexitoq1001@gmail.com>
2025-06-25 21:47:28 -0500

    version con password hasheada

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -2,6 +2,8 @@
 from app.models.user_model import User
 from app.models import db
 from flask import jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 def register_user(data):
     username = data.get("username")
@@ -12,8 +14,10 @@ def register_user(data):
 
     if User.query.filter_by(username=username).first():
         return {"error": "El usuario ya existe"}, 409
+    
+    hashed_password = generate_password_hash(password)
 
-    new_user = User(username=username, password=password)
+    new_user = User(username=username, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
 
@@ -25,10 +29,11 @@ def login_user(data):
     username = data.get("username")
     password = data.get("password")
 
-    user = User.query.filter_by(username=username, password=password).first()
+    user = User.query.filter_by(username=username).first()
 
-    if not user:
+    if not user or not check_password_hash(user.password, password):
         return {"error": "Credenciales inválidas"}, 401
 
-    print("Credenciales validas")
+    print("Credenciales válidas")
     return {"message": "Inicio de sesión exitoso", "user": user.to_dict()}, 200
+
